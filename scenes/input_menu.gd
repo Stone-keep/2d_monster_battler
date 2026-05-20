99a640ca2d745400163d0da6acd01767a5381a1a
Stone-keep <radek12345@gmail.com>
2026-05-20 20:56:54 +0200

    Handle menu transitions

--- scenes/input_menu.gd
+++ scenes/input_menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 var grid_button_scene = preload("res://scenes/grid_button.tscn")
 var list_button_scene = preload("res://scenes/list_button.tscn")
+
 const main_buttons = {
 	Global.State.ATTACK: "Attack",
 	Global.State.DEFEND: "Defend",
@@ -9,12 +10,15 @@ const main_buttons = {
 	Global.State.ITEM: "Item",
 }
 
+var current_state: Global.State: set = state_handler
+
 func _ready() -> void:
-	#create_attack_buttons()
-	#create_grid_buttons(Global.State.MAIN, main_buttons)
-	#create_swap_buttons()
-	create_item_buttons()
-	
+	create_grid_buttons(Global.State.MAIN, main_buttons)
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		current_state = Global.State.MAIN
+
 
 func create_grid_buttons(state: Global.State, data: Dictionary):
 	for button in $GridMenu.get_children():
@@ -24,6 +28,8 @@ func create_grid_buttons(state: Global.State, data: Dictionary):
 		grid_button.setup(state, key, data[key])
 		$GridMenu.add_child(grid_button)
 		grid_button.connect("press", button_handler)
+	await get_tree().process_frame
+	$GridMenu.get_child(0).grab_focus()
 
 func create_list_buttons(state: Global.State, data: Dictionary):
 	for button in $ScrollContainer/ListMenu.get_children():
@@ -33,6 +39,8 @@ func create_list_buttons(state: Global.State, data: Dictionary):
 		list_button.setup(state, d, data)
 		$ScrollContainer/ListMenu.add_child(list_button)
 		list_button.connect("press", button_handler)
+	await get_tree().process_frame
+	$ScrollContainer/ListMenu.get_child(0).grab_focus()
 
 func create_attack_buttons():
 	var current_monster_attacks = Global.monster_data[Global.current_monster]["attacks"]
@@ -55,6 +63,31 @@ func create_item_buttons():
 		owned_item_data[item] = Global.item_data[item]
 	create_list_buttons(Global.State.ITEM, owned_item_data)
 
+func handle_monster_defend():
+	print("Monster defends")
 
 func button_handler(state, type):
-	print("test")
+	if state == Global.State.MAIN:
+		current_state = type
+
+func state_handler(value):
+	current_state = value
+	match value:
+		Global.State.MAIN:
+			$GridMenu.show()
+			$ScrollContainer.hide()
+			create_grid_buttons(Global.State.MAIN, main_buttons)
+		Global.State.ATTACK:
+			$GridMenu.show()
+			$ScrollContainer.hide()
+			create_attack_buttons()
+		Global.State.DEFEND:
+			handle_monster_defend()
+		Global.State.SWAP:
+			$GridMenu.hide()
+			$ScrollContainer.show()
+			create_swap_buttons()
+		Global.State.ITEM:
+			$GridMenu.hide()
+			$ScrollContainer.show()
+			create_item_buttons()
--- scenes/list_button.gd
+++ scenes/list_button.gd
@@ -4,11 +4,11 @@ var state
 var type
 signal press(state, type)
 
+func _on_pressed() -> void:
+	press.emit(state, type)
+
 func setup(menu_state, button_type, data):
 	state = menu_state
 	type = button_type
 	$HBoxContainer/Label.text = data[button_type]["name"]
 	$HBoxContainer/TextureRect.texture = load(data[button_type]["icon"])
-
-func _on_pressed() -> void:
-	press.emit(state, type)
