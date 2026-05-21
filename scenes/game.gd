681550e5b94b1ec0b037220474dea1703e0b50cd
Stone-keep <radek12345@gmail.com>
2026-05-21 16:37:59 +0200

    Add enemy monster setup and animation

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,14 +1,29 @@
 extends Control
 
+@export var animation_index := 0:
+	set(value):
+		animation_index = value
+		var atlas = $Monsters/EnemyMonster.texture as AtlasTexture
+		atlas.region.position = Vector2(96 * animation_index, 0)
 
 func _ready() -> void:
+	# Player Monster Setup
 	Global.current_monster = Global.monsters[0]
+	$Monsters/PlayerMonster.texture = load(Global.monster_data[Global.current_monster]["back texture"])
+
+	# Enemy Monster Setup
+	Global.current_enemy = Global.Monster.values().pick_random()
+	var enemy_atlas: AtlasTexture = AtlasTexture.new()
+	enemy_atlas.atlas = load(Global.monster_data[Global.current_enemy]["front texture"])
+	enemy_atlas.region.size = Vector2(96, 96)
+	$Monsters/EnemyMonster.texture = enemy_atlas
 
 func _on_input_menu_selected(state: int, type: Variant) -> void:
 	match state:
 		Global.State.ATTACK:
 			var target = $Monsters/EnemyMonster if Global.attack_data[type]["target"] else $Monsters/PlayerMonster
 			attack(target, type)
+			$InputMenu.current_state = Global.State.MAIN
 		Global.State.SWAP:
 			Global.current_monster = type
 			$Monsters/PlayerMonster.texture = load(Global.monster_data[type]["back texture"])
