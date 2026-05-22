795b85e2e00d52b876651ee1ab8d6cc71b88f546
Stone-keep <radek12345@gmail.com>
2026-05-22 17:25:26 +0200

    Add button and status bar styling

--- scenes/monster_status.gd
+++ scenes/monster_status.gd
@@ -1,17 +1,20 @@
 extends Control
 
+@onready var monster_name: Label = $PanelContainer/MarginContainer/VBoxContainer/Label
+@onready var health_bar: TextureProgressBar = $PanelContainer/MarginContainer/VBoxContainer/ProgressBar
+
 func setup(is_player: bool):
 	if is_player:
-		$VBoxContainer/Label.text = Global.monster_data[Global.current_monster]["name"]
-		$VBoxContainer/ProgressBar.max_value = Global.monster_data[Global.current_monster]["max health"]
-		$VBoxContainer/ProgressBar.value = Global.monster_data[Global.current_monster]["max health"]
+		monster_name.text = Global.monster_data[Global.current_monster]["name"]
+		health_bar.max_value = Global.monster_data[Global.current_monster]["max health"]
+		health_bar.value = Global.monster_data[Global.current_monster]["max health"]
 	else:
-		$VBoxContainer/Label.text = Global.monster_data[Global.current_enemy]["name"]
-		$VBoxContainer/ProgressBar.max_value = Global.monster_data[Global.current_enemy]["max health"]
-		$VBoxContainer/ProgressBar.value = Global.monster_data[Global.current_enemy]["max health"]
+		monster_name.text = Global.monster_data[Global.current_enemy]["name"]
+		health_bar.max_value = Global.monster_data[Global.current_enemy]["max health"]
+		health_bar.value = Global.monster_data[Global.current_enemy]["max health"]
 
 func update(attack_damage):
-	$VBoxContainer/ProgressBar.value -= attack_damage
+	health_bar.value -= attack_damage
 
 func is_defeated():
-	return $VBoxContainer/ProgressBar.value <= 0
+	return health_bar.value <= 0
